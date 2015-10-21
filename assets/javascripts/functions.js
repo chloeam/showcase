12d45be6328833c934610c10f16ae1292b062a08
Chloe Atchue-Mamlet <chloe793@gmail.com>
2015-10-21 10:02:29 -0400

    set up javascript, portfolio page hover effect

---
+++ assets/javascripts/functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+  //$( ".portfolio-project" ).hover(function() {
+    //$( ".portfolio-cover" ).fadeOut( 100 );
+    //$( ".portfolio-cover" ).fadeIn( 500 );
+  //});
+  
+}); 
